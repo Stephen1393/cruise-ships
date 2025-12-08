96ec2f9cefdafab87f28a21caa18a12fa6a7dbd9
Stephen1393 <stephen.bending20@gmail.com>
2025-12-08 16:16:19 +0000

    Added addShip and removeShip methods and created tests.

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -1,3 +1,5 @@
+const Ship = require('../src/cruise-ship.js');
+const Itinerary = require('../src/Itinerary.js');
 const Port = require('../src/Port.js')
 
 describe('Port', () => {
@@ -13,3 +15,27 @@ describe('Port', () => {
         expect(port.name).toBe('Dover')
     })
 });
+
+    describe('addShip', () => {
+        it('If a ship is docked, it adds ship to an array', () => {
+            const port = new Port("Dover");
+            const itinerary = new Itinerary([port]);
+            const ship = new Ship(itinerary)
+
+            port.addShip(ship)
+
+            expect(port.ships).toContain(port.ships[0])
+        });
+    });
+        it('removeShip', () => {
+             const port = new Port("Dover");
+            const itinerary = new Itinerary([port]);
+            const shipA = new Ship(itinerary)
+            const shipB = new Ship(itinerary)
+
+            port.addShip(shipA,shipB)
+
+            port.removeShip(shipB)
+
+            expect(port.ships).not.toContain(port.ships[shipB])
+        });
--- src/Port.js
+++ src/Port.js
@@ -1,7 +1,19 @@
 class Port {
     constructor(name) {
         this.name = name
+        this.ships = []
+       
     }
+addShip(ship) {
+    this.ships.push(ship)
+}
+
+removeShip(ship) {
+this.ships = this.ships.filter(function (currentShip) {
+    return currentShip != ship
+});
+}
+
 }
 
 module.exports = Port
