623024cf5b659bc4f843015c70a455bc9698d939
Stephen1393 <stephen.bending20@gmail.com>
2025-12-12 10:52:42 +0000

    Updated stubs and spies/moved to beforeEach.

--- __tests__/cruise-Ship.test.js
+++ __tests__/cruise-Ship.test.js
@@ -9,8 +9,21 @@ describe('ship', () => {
     let port;
 
     beforeEach(() => {
-            port = new Port('Dover'); 
-            itinerary = new Itinerary([port]) 
+        dover = {
+            name:"Dover",
+            addShip:jest.fn(),
+            removeShip:jest.fn(),
+            ships: []
+        };
+
+        exeter = {
+            name:"Exeter",
+            addShip:jest.fn(),
+            removeShip:jest.fn(),
+            ships: []
+        };
+
+            itinerary = {ports:[dover,exeter]} 
             ship = new Ship(itinerary)  
         });
     
@@ -24,28 +37,19 @@ describe('ship', () => {
     });
         
         it('gets added to port on instantiation', () => { 
-            const port = {name:"Dover", addShip:jest.fn()}
-            const itinerary = {ports:[port]}
-            const ship = new Ship(itinerary)
-            
-
-
-            expect(port.addShip).toHaveBeenCalledTimes(1)
-            expect(port.addShip).toHaveBeenCalledWith(ship)
+            expect(dover.addShip).toHaveBeenCalledTimes(1)
+            expect(dover.addShip).toHaveBeenCalledWith(ship)
         }); 
 
 
      
      describe('setSail', () => { 
         it('checks whether ship atSea is false, and changes to true if so.', () => { 
-            const port = {name:"Exeter", removeShip:jest.fn()}
-            ship.currentPort = port
-            
             ship.setSail()
 
             expect(ship.atSea).toBe(true) 
-            expect(port.removeShip).toHaveBeenCalledWith(ship)
-            expect(port.removeShip).toHaveBeenCalledTimes(1)
+            expect(dover.removeShip).toHaveBeenCalledWith(ship)
+            expect(dover.removeShip).toHaveBeenCalledTimes(1)
         });
     });
 }); 
@@ -58,26 +62,31 @@ describe('Sailing between multiply ports', () => {
     let itinerary;
     let ship;
     
-    beforeEach(() => {
-        dover = new Port('dover'); 
-        exeter = new Port('Exeter'); 
-        itinerary = new Itinerary([dover,exeter]); 
-        ship = new Ship(itinerary)
-    });
+    beforeEach(() => {  
+        dover = {
+            name:"Dover",
+            addShip:jest.fn(),
+            removeShip:jest.fn(),
+            ships: []
+        };
+
+        exeter = {
+            name:"Exeter",
+            addShip:jest.fn(),
+            removeShip:jest.fn(),
+            ships: []
+        };
+
+            itinerary = {ports:[dover,exeter]} 
+            ship = new Ship(itinerary)  
+        });
     
     it('Can dock at a different port', () => {
-        const port = {name:"Dover", addShip:jest.fn(), removeShip:jest.fn()}
-        const port2 = {name:"Exeter", addShip:jest.fn()}
-
-        ship.currentPort = port
-        itinerary.ports[1] = port2
-
         ship.setSail() 
-
         ship.dock() 
 
-        expect(port2.addShip).toHaveBeenCalledTimes(1)
-        expect(port2.addShip).toHaveBeenCalledWith(ship)
+        expect(exeter.addShip).toHaveBeenCalledTimes(1)
+        expect(exeter.addShip).toHaveBeenCalledWith(ship)
         expect(ship.atSea).toBe(false); 
     });
 });
